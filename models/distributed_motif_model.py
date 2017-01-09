27d1f93471d041e1d4e1f9d9ed297b1ffe1e8887
peter-koo <peter_koo@harvard.edu>
2017-01-08 20:53:09 -0500

    updates

--- models/distributed_motif_model.py
+++ models/distributed_motif_model.py
@@ -28,12 +28,14 @@ def model(shape, num_labels):
 	    return net
 
 
+
 	input_var = T.tensor4('inputs')
 	target_var = T.dmatrix('targets')
+		
 	
 	net = {}
 	net['input'] = layers.InputLayer(input_var=input_var, shape=shape)
-	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=30, filter_size=(19, 1), stride=(1, 1),    # 1000
+	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=30, filter_size=(19, 1), stride=(1, 1),    # 246
 	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='same')
 	net['conv1_norm'] = layers.BatchNormLayer(net['conv1'])
 	net['conv1_active'] = layers.NonlinearityLayer(net['conv1_norm'], nonlinearity=nonlinearities.rectify)
@@ -41,7 +43,7 @@ def model(shape, num_labels):
 	net = residual_block(net, 'conv1_dropout1', 'conv1_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
 	net['conv1_dropout'] = layers.DropoutLayer(net['conv1_2_resid'], p=0.1)
 
-	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=64, filter_size=(5, 1), stride=(4, 1), # 246
+	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=100, filter_size=(8, 1), stride=(5, 1), # 61
 	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
 	net['conv2_norm'] = layers.BatchNormLayer(net['conv2'])
 	net['conv2_active'] = layers.NonlinearityLayer(net['conv2_norm'], nonlinearity=nonlinearities.rectify)
@@ -49,7 +51,7 @@ def model(shape, num_labels):
 	net = residual_block(net, 'conv2_dropout1', 'conv2_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
 	net['conv2_dropout'] = layers.DropoutLayer(net['conv2_2_resid'], p=0.1)
 
-	net['conv3'] = layers.Conv2DLayer(net['conv2_dropout'], num_filters=128, filter_size=(5, 1), stride=(4, 1),  # 78
+	net['conv3'] = layers.Conv2DLayer(net['conv2_dropout'], num_filters=250, filter_size=(8, 1), stride=(5, 1),  # 15
 	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
 	net['conv3_norm'] = layers.BatchNormLayer(net['conv3'])
 	net['conv3_active'] = layers.NonlinearityLayer(net['conv3_norm'], nonlinearity=nonlinearities.rectify)
@@ -57,7 +59,7 @@ def model(shape, num_labels):
 	net = residual_block(net, 'conv3_dropout1', 'conv3_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
 	net['conv3_dropout'] = layers.DropoutLayer(net['conv3_2_resid'], p=0.1)
 
-	net['conv4'] = layers.Conv2DLayer(net['conv3_dropout'], num_filters=256, filter_size=(5, 1), stride=(4, 1), # 21
+	net['conv4'] = layers.Conv2DLayer(net['conv3_dropout'], num_filters=500, filter_size=(8, 1), stride=(5, 1), # 3
 	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
 	net['conv4_norm'] = layers.BatchNormLayer(net['conv4'])
 	net['conv4_active'] = layers.NonlinearityLayer(net['conv4_norm'], nonlinearity=nonlinearities.rectify)
@@ -65,16 +67,12 @@ def model(shape, num_labels):
 	net = residual_block(net, 'conv4_dropout1', 'conv4_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
 	net['conv4_dropout'] = layers.DropoutLayer(net['conv4_2_resid'], p=0.1)
 
-	net['conv5'] = layers.Conv2DLayer(net['conv4_dropout'], num_filters=512, filter_size=(5, 1), stride=(4, 1), # 3
-	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
-	net['conv5_norm'] = layers.BatchNormLayer(net['conv5'])
-	net['conv5_active'] = layers.NonlinearityLayer(net['conv5_norm'], nonlinearity=nonlinearities.rectify)
-	net['conv5_dropout'] = layers.DropoutLayer(net['conv5_active'], p=0.1)
+	net['conv5'] = layers.Conv2DLayer(net['conv4_dropout'], num_filters=num_labels, filter_size=(7, 1), stride=(1, 1), # 3
+	                                    W=init.HeNormal(), b=init.Constant(0.05), nonlinearity=None, pad='valid')
+	net['conv5_active'] = layers.NonlinearityLayer(net['conv5'], nonlinearity=nonlinearities.sigmoid)
+	
+	net['output'] = layers.ReshapeLayer(net['conv5_active'], [-1, num_labels])
 
-	net['conv6'] = layers.Conv2DLayer(net['conv5_dropout'], num_filters=num_labels, filter_size=(3, 1), stride=(1, 1), # 3
-	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
-	net['conv6_active'] = layers.NonlinearityLayer(net['conv6'], nonlinearity=nonlinearities.sigmoid)
-	net['output'] = layers.ReshapeLayer(net['conv6_active'], [-1, num_labels])
 
 
 	# optimization parameters
@@ -88,6 +86,123 @@ def model(shape, num_labels):
 
 
 """
+
+
+	net = {}
+	net['input'] = layers.InputLayer(input_var=input_var, shape=shape)
+	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=25, filter_size=(19, 1), stride=(1, 1),    # 246
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='same')
+	net['conv1_norm'] = layers.BatchNormLayer(net['conv1'])
+	net['conv1_active'] = layers.NonlinearityLayer(net['conv1_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout1'] = layers.DropoutLayer(net['conv1_active'], p=0.1)
+	net = residual_block(net, 'conv1_dropout1', 'conv1_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout'] = layers.DropoutLayer(net['conv1_2_resid'], p=0.1)
+
+	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=50, filter_size=(5, 1), stride=(2, 1), # 61
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv2_norm'] = layers.BatchNormLayer(net['conv2'])
+	net['conv2_active'] = layers.NonlinearityLayer(net['conv2_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout1'] = layers.DropoutLayer(net['conv2_active'], p=0.1)
+	net = residual_block(net, 'conv2_dropout1', 'conv2_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout'] = layers.DropoutLayer(net['conv2_2_resid'], p=0.1)
+
+	net['conv3'] = layers.Conv2DLayer(net['conv2_dropout'], num_filters=100, filter_size=(5, 1), stride=(2, 1),  # 15
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv3_norm'] = layers.BatchNormLayer(net['conv3'])
+	net['conv3_active'] = layers.NonlinearityLayer(net['conv3_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout1'] = layers.DropoutLayer(net['conv3_active'], p=0.1)
+	net = residual_block(net, 'conv3_dropout1', 'conv3_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout'] = layers.DropoutLayer(net['conv3_2_resid'], p=0.1)
+
+	net['conv4'] = layers.Conv2DLayer(net['conv3_dropout'], num_filters=200, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv4_norm'] = layers.BatchNormLayer(net['conv4'])
+	net['conv4_active'] = layers.NonlinearityLayer(net['conv4_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout1'] = layers.DropoutLayer(net['conv4_active'], p=0.1)
+	net = residual_block(net, 'conv4_dropout1', 'conv4_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout'] = layers.DropoutLayer(net['conv4_2_resid'], p=0.1)
+
+	net['conv5'] = layers.Conv2DLayer(net['conv4_dropout'], num_filters=400, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv5_norm'] = layers.BatchNormLayer(net['conv5'])
+	net['conv5_active'] = layers.NonlinearityLayer(net['conv5_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv5_dropout1'] = layers.DropoutLayer(net['conv5_active'], p=0.1)
+	net = residual_block(net, 'conv5_dropout1', 'conv45_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv5_dropout'] = layers.DropoutLayer(net['conv54_2_resid'], p=0.1)
+
+	net['conv6'] = layers.Conv2DLayer(net['conv5_dropout'], num_filters=600, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv6_norm'] = layers.BatchNormLayer(net['conv6'])
+	net['conv6_active'] = layers.NonlinearityLayer(net['conv4_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv6_dropout1'] = layers.DropoutLayer(net['conv6_active'], p=0.1)
+	net = residual_block(net, 'conv6_dropout1', 'conv6_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv6_dropout'] = layers.DropoutLayer(net['conv6_2_resid'], p=0.1)
+
+	net['conv7'] = layers.Conv2DLayer(net['conv6_dropout'], num_filters=800, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv7_norm'] = layers.BatchNormLayer(net['conv7'])
+	net['conv7_active'] = layers.NonlinearityLayer(net['conv7_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv7_dropout'] = layers.DropoutLayer(net['conv7_active'], p=0.1)
+
+	net['dense'] = layers.DenseLayer(net['conv7_dropout'], num_units=500, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.rectify)
+	net['dense_dropout'] = layers.DropoutLayer(net['dense'], p=0.2)
+
+	net['output'] = layers.DenseLayer(net['dense_dropout'], num_units=num_labels, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.sigmoid)
+
+
+
+	net = {}
+	net['input'] = layers.InputLayer(input_var=input_var, shape=shape)
+	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=25, filter_size=(19, 1), stride=(1, 1),    # 246
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='same')
+	net['conv1_norm'] = layers.BatchNormLayer(net['conv1'])
+	net['conv1_active'] = layers.NonlinearityLayer(net['conv1_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout1'] = layers.DropoutLayer(net['conv1_active'], p=0.1)
+	net = residual_block(net, 'conv1_dropout1', 'conv1_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout'] = layers.DropoutLayer(net['conv1_2_resid'], p=0.1)
+
+	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=50, filter_size=(5, 1), stride=(5, 1), # 61
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv2_norm'] = layers.BatchNormLayer(net['conv2'])
+	net['conv2_active'] = layers.NonlinearityLayer(net['conv2_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout1'] = layers.DropoutLayer(net['conv2_active'], p=0.1)
+	net = residual_block(net, 'conv2_dropout1', 'conv2_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout'] = layers.DropoutLayer(net['conv2_2_resid'], p=0.1)
+
+	net['conv3'] = layers.Conv2DLayer(net['conv2_dropout'], num_filters=100, filter_size=(5, 1), stride=(5, 1),  # 15
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv3_norm'] = layers.BatchNormLayer(net['conv3'])
+	net['conv3_active'] = layers.NonlinearityLayer(net['conv3_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout1'] = layers.DropoutLayer(net['conv3_active'], p=0.1)
+	net = residual_block(net, 'conv3_dropout1', 'conv3_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout'] = layers.DropoutLayer(net['conv3_2_resid'], p=0.1)
+
+	net['conv4'] = layers.Conv2DLayer(net['conv3_dropout'], num_filters=200, filter_size=(5, 1), stride=(5, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv4_norm'] = layers.BatchNormLayer(net['conv4'])
+	net['conv4_active'] = layers.NonlinearityLayer(net['conv4_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout1'] = layers.DropoutLayer(net['conv4_active'], p=0.1)
+	net = residual_block(net, 'conv4_dropout1', 'conv4_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout'] = layers.DropoutLayer(net['conv4_2_resid'], p=0.1)
+
+	net['conv5'] = layers.Conv2DLayer(net['conv4_dropout'], num_filters=200, filter_size=(5, 1), stride=(4, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv5_norm'] = layers.BatchNormLayer(net['conv5'])
+	net['conv5_active'] = layers.NonlinearityLayer(net['conv5_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv5_dropout'] = layers.DropoutLayer(net['conv5_active'], p=0.1)
+
+	net['dense'] = layers.DenseLayer(net['conv5_dropout'], num_units=500, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.rectify)
+	net['dense_dropout'] = layers.DropoutLayer(net['dense'], p=0.2)
+
+	net['output'] = layers.DenseLayer(net['dense_dropout'], num_units=num_labels, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.sigmoid)
+
+
+
+
 	net = {}
 	net['input'] = layers.InputLayer(input_var=input_var, shape=shape)
 	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=30, filter_size=(19, 1), stride=(1, 1),    # 1000
@@ -140,6 +255,79 @@ def model(shape, num_labels):
 	net['conv6_active'] = layers.NonlinearityLayer(net['conv6'], nonlinearity=nonlinearities.sigmoid)
 
 	net['output'] = layers.ReshapeLayer(net['conv6_active'], [-1, num_labels])
+
+	net = {}
+	net['input'] = layers.InputLayer(input_var=input_var, shape=shape)
+	net['conv1'] = layers.Conv2DLayer(net['input'], num_filters=25, filter_size=(19, 1), stride=(1, 1),    # 246
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='same')
+	net['conv1_norm'] = layers.BatchNormLayer(net['conv1'])
+	net['conv1_active'] = layers.NonlinearityLayer(net['conv1_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout1'] = layers.DropoutLayer(net['conv1_active'], p=0.1)
+	net = residual_block(net, 'conv1_dropout1', 'conv1_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv1_dropout'] = layers.DropoutLayer(net['conv1_2_resid'], p=0.1)
+
+	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=50, filter_size=(5, 1), stride=(2, 1), # 61
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv2_norm'] = layers.BatchNormLayer(net['conv2'])
+	net['conv2_active'] = layers.NonlinearityLayer(net['conv2_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout1'] = layers.DropoutLayer(net['conv2_active'], p=0.1)
+	net = residual_block(net, 'conv2_dropout1', 'conv2_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv2_dropout'] = layers.DropoutLayer(net['conv2_2_resid'], p=0.1)
+
+	net['conv3'] = layers.Conv2DLayer(net['conv2_dropout'], num_filters=100, filter_size=(5, 1), stride=(2, 1),  # 15
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv3_norm'] = layers.BatchNormLayer(net['conv3'])
+	net['conv3_active'] = layers.NonlinearityLayer(net['conv3_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout1'] = layers.DropoutLayer(net['conv3_active'], p=0.1)
+	net = residual_block(net, 'conv3_dropout1', 'conv3_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv3_dropout'] = layers.DropoutLayer(net['conv3_2_resid'], p=0.1)
+
+	net['conv4'] = layers.Conv2DLayer(net['conv3_dropout'], num_filters=150, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv4_norm'] = layers.BatchNormLayer(net['conv4'])
+	net['conv4_active'] = layers.NonlinearityLayer(net['conv4_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout1'] = layers.DropoutLayer(net['conv4_active'], p=0.1)
+	net = residual_block(net, 'conv4_dropout1', 'conv4_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv4_dropout'] = layers.DropoutLayer(net['conv4_2_resid'], p=0.1)
+
+	net['conv5'] = layers.Conv2DLayer(net['conv4_dropout'], num_filters=200, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv5_norm'] = layers.BatchNormLayer(net['conv5'])
+	net['conv5_active'] = layers.NonlinearityLayer(net['conv5_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv5_dropout1'] = layers.DropoutLayer(net['conv5_active'], p=0.1)
+	net = residual_block(net, 'conv5_dropout1', 'conv5_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv5_dropout'] = layers.DropoutLayer(net['conv5_2_resid'], p=0.1)
+
+	net['conv6'] = layers.Conv2DLayer(net['conv5_dropout'], num_filters=250, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv6_norm'] = layers.BatchNormLayer(net['conv6'])
+	net['conv6_active'] = layers.NonlinearityLayer(net['conv6_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv6_dropout1'] = layers.DropoutLayer(net['conv6_active'], p=0.1)
+	net = residual_block(net, 'conv6_dropout1', 'conv6_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv6_dropout'] = layers.DropoutLayer(net['conv6_2_resid'], p=0.1)
+
+	net['conv7'] = layers.Conv2DLayer(net['conv6_dropout'], num_filters=350, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv7_norm'] = layers.BatchNormLayer(net['conv7'])
+	net['conv7_active'] = layers.NonlinearityLayer(net['conv7_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv7_dropout1'] = layers.DropoutLayer(net['conv7_active'], p=0.1)
+	net = residual_block(net, 'conv7_dropout1', 'conv7_2', filter_size=(5,1), nonlinearity=nonlinearities.rectify)
+	net['conv7_dropout'] = layers.DropoutLayer(net['conv7_2_resid'], p=0.1)
+
+	net['conv8'] = layers.Conv2DLayer(net['conv7_dropout'], num_filters=500, filter_size=(5, 1), stride=(2, 1), # 3
+	                                    W=init.HeNormal(), b=None, nonlinearity=None, pad='valid')
+	net['conv8_norm'] = layers.BatchNormLayer(net['conv8'])
+	net['conv8_active'] = layers.NonlinearityLayer(net['conv8_norm'], nonlinearity=nonlinearities.rectify)
+	net['conv8_dropout'] = layers.DropoutLayer(net['conv8_active'], p=0.1)
+
+	net['dense'] = layers.DenseLayer(net['conv8_dropout'], num_units=500, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.rectify)
+	net['dense_dropout'] = layers.DropoutLayer(net['dense'], p=0.2)
+
+	net['output'] = layers.DenseLayer(net['dense_dropout'], num_units=num_labels, W=init.HeNormal(), 
+	                                  b=init.Constant(0.05), nonlinearity=nonlinearities.sigmoid)
+
+
 
 
 """
--- models/local_motif_model.py
+++ models/local_motif_model.py
@@ -13,14 +13,14 @@ def model(shape, num_labels):
 					 W=init.GlorotUniform(), b=None, nonlinearity=None, pad='same', flip_filters=False)
 	net['conv1_norm'] = layers.BatchNormLayer(net['conv1'])
 	net['conv1_active'] = layers.NonlinearityLayer(net['conv1_norm'], nonlinearity=nonlinearities.rectify)
-	net['conv1_pool'] = layers.MaxPool2DLayer(net['conv1_active'], pool_size=(40, 1), stride=(40, 1), ignore_border=False) # 17
+	net['conv1_pool'] = layers.MaxPool2DLayer(net['conv1_active'], pool_size=(40, 1), stride=(40, 1), ignore_border=False) # 25
 	net['conv1_dropout'] = layers.DropoutLayer(net['conv1_pool'], p=0.1)
 
-	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=64, filter_size=(8, 1), stride=(1, 1), # 20
+	net['conv2'] = layers.Conv2DLayer(net['conv1_dropout'], num_filters=50, filter_size=(6, 1), stride=(1, 1), # 20
 										W=init.GlorotUniform(), b=None, nonlinearity=None, pad='valid', flip_filters=False)
 	net['conv2_norm'] = layers.BatchNormLayer(net['conv2'])
 	net['conv2_active'] = layers.NonlinearityLayer(net['conv2_norm'], nonlinearity=nonlinearities.rectify)
-	net['conv2_pool'] = layers.MaxPool2DLayer(net['conv2_active'], pool_size=(18, 1), stride=(18, 1), ignore_border=False) # 3
+	net['conv2_pool'] = layers.MaxPool2DLayer(net['conv2_active'], pool_size=(20, 1), stride=(20, 1), ignore_border=False) # 3
 	net['conv2_dropout'] = layers.DropoutLayer(net['conv2_pool'], p=0.1)
 
 	#net['conv3'] = layers.DenseLayer(net['conv2_dropout'], num_units=num_labels, W=init.GlorotUniform(), b=init.Constant(), nonlinearity=None)
